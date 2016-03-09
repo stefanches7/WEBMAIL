ea7ba1adcee187983da1e5693a6701f7b4ef5f72
stefanches7 <xxx-man7@mail.ru>
2016-03-09 23:15:50 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
 sudo apt-get install nginx
-sudo ln -sf /home/box/web/etc/nginx.conf  etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /home/box/web/etc/nginx/sites-enabled/test.conf
 sudo etc/init.d/nginx restart
