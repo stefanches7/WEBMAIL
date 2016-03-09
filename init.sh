8e7f53a2fe46f9c7aeeeb1919cd1f35bf3f9048c
stefanches7 <xxx-man7@mail.ru>
2016-03-10 00:04:36 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo service nginx restart
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
