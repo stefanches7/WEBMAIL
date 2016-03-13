dfc86d7a2a4cb126e90b205f6b03bdd150a6c579
stefanches7 <xxx-man7@mail.ru>
2016-03-13 15:17:53 +0300

    New init

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
+sudo pip install Werkzeug
+sudo pip install Flask
 sudo ln -sf /home/box/web/conf/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/hello.py /usr/local/lib/python2.7/hello.py
 sudo /etc/init.d/gunicorn restart
 gunicorn -b 0.0.0.0:8080 hello:app &
