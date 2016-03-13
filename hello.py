3afc11840bc737fa4132a1ea3f54bfced3ce7be5
stefanches7 <xxx-man7@mail.ru>
2016-03-13 19:03:11 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,6 +4,5 @@ def app(environ, start_response):
   response = ''
   for query_item in lst:
     response+=query_item+"\n"
-  start_reponse('200 OK', [('Content-Type', 'text/plain'),("Content-Length",len(response)*8)])
-  return response
+  return ('200 OK', [('Content-Type', 'text/plain'),("Content-Length",len(response)*8)], [response])
   
