63475319b488cf774e0dcebc130cf4a56dd8023e
stefanches7 <xxx-man7@mail.ru>
2016-03-13 14:52:59 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 def myapp(environ, start_response):
   lst = environ["QUERY_STRING"].split('&')
+  response = ''
+  for query_item in lst:
+    s+=query_item+"\n"
   start_reponse("200 OK", [("Content-Type", "text/plain"),("Content-Length",str(len(lst)))])
-  return iter(lst)
+  return response
   
