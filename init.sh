0eb2c654e18355a3ce897b43f22ecf50da8787bd
stefanches7 <xxx-man7@mail.ru>
2016-03-13 13:40:02 +0300

    INIT
    NGINX launch + GUNICORN launching /home/box/web/hello.py WSGI app

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo mv /home/box/web/hello_config.py /home/box/etc
+sudo mv /home/box/web/hello_config.py /home/box/etc/
 sudo ln -sf /home/box/etc/hello_config.py  /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
+gunicorn /home/box/web/hello.py
