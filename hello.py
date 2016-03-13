b7720ecfb9608476c1baeed562a69d513b229814
stefanches7 <xxx-man7@mail.ru>
2016-03-13 15:54:27 +0300

    messin' with 'Content-Type'

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ def myapp(environ, start_response):
   response = ''
   for query_item in lst:
     s+=query_item+"\n"
-  start_reponse("200 OK", [("Content-Type", "text/plain"),("Content-Length",str(len(lst)))])
+  start_reponse('200 OK', [('Content-Type', 'text/plain'),("Content-Length",len(response)*8)])
   return response
   
 app.wsgi_app = ProxyFix(app.wsgi_app)
