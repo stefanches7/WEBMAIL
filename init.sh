994895bc8f2b374f0ab83121cd6c749caec9d27d
stefanches7 <xxx-man7@mail.ru>
2016-03-13 17:20:53 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,4 +5,4 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/conf/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/hello.py /usr/local/lib/python2.7/hello.py
 sudo /etc/init.d/gunicorn restart
-gunicorn -b 0.0.0.0:8080 hello:app --access-logfile logs/guni.access.log --log-file logs/guni.log &
+sudo gunicorn -b 0.0.0.0:8080 hello:app --access-logfile logs/guni.access.log --log-file logs/guni.log &
