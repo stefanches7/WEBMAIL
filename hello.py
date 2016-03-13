9f7fa9fc475659747280ffbe5ce83dd3365e741a
stefanches7 <xxx-man7@mail.ru>
2016-03-13 16:16:59 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,3 @@
-from werkzeug.contrib.fixers import ProxyFix
 
 def app(environ, start_response):
   lst = environ["QUERY_STRING"].split('&')
@@ -8,5 +7,3 @@ def app(environ, start_response):
   start_reponse('200 OK', [('Content-Type', 'text/plain'),("Content-Length",len(response)*8)])
   return response
   
-app.wsgi_app = ProxyFix(app.wsgi_app)
-app.run()
