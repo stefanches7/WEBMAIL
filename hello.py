7282ac6b105aac42926a269f9d65f25dc71b634d
stefanches7 <xxx-man7@mail.ru>
2016-03-13 15:08:14 +0300

    Gunicorn-callable app

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
+from werkzeug.contrib.fixers import ProxyFix
+from flask import Flask
+app = Flask(__name__)
 def myapp(environ, start_response):
   lst = environ["QUERY_STRING"].split('&')
   response = ''
@@ -6,3 +9,7 @@ def myapp(environ, start_response):
   start_reponse("200 OK", [("Content-Type", "text/plain"),("Content-Length",str(len(lst)))])
   return response
   
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+if __name__ == '__main__':
+    app.run()
